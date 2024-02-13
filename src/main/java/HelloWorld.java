b8147cceafab75a9b76ce6ecf679a7a4512508dd
Andrey_Zaikin <andrewgit7@gmail.com>
2024-02-13 06:45:37 +0600

    done

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,3 +1,7 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component("helloworld")
 public class HelloWorld {
  
     private String message;
